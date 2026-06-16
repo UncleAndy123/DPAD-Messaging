751f4100c940bcfafc70025fb6794c577eda96ff
jbriones95 <108902016+jbriones95@users.noreply.github.com>
2026-06-16 12:44:35 -0600

    chore: bump version to 0.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.dpadsms"
         minSdk = 23
         targetSdk = 34
-        versionCode = 10
-        versionName = "0.1.1"
+        versionCode = 11
+        versionName = "0.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
